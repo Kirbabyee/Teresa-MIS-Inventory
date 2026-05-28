37fa8a8e60bfdee00a746bc5bac27c550c82109e
Lei <ucc.atay.leocarlo@gmail.com>
2026-05-28 19:52:50 +0800

    file cleanup

--- temp-parse2.mjs
+++
@@ -1,8 +0,0 @@
-import * as parser from "@babel/parser";
-const src = "function Test(){ return (<div className=\"x\">Hi</div>); }";
-try {
-  parser.parse(src, { sourceType: "module", plugins: ["jsx"] });
-  console.log("BABEL_OK");
-} catch (e) {
-  console.error(e.message);
-}
